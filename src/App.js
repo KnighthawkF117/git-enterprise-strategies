ae0dbe61111106527c15cb9f885232f613c9af77
KnighthawkF117 <73857479+KnighthawkF117@users.noreply.github.com>
2023-01-25 12:40:22 -0500

    added new project text (#3)
    Co-authored-by: samilieberman <samilieberman1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Omar Heriba and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Hello World! My name is Omar Heriba and this is my project 
+        </p>
       </header>
     </div>
   );
